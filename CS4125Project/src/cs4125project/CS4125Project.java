13f301ea5663612594d2070cf8aa0d27139718ac
Brian Archbold <brianarchbold96@yahoo.ie>
2018-10-24 20:13:07 +0100

    Implement working Payment system for a customer
    Payment system uses a Bridge design pattern to abstract Payment types
    and their providers. This allows an easier method of injecting more
    providers and payment types into the system
    
    Signed-off-by: Brian Archbold <brianarchbold96@yahoo.ie>

--- CS4125Project/src/cs4125project/CS4125Project.java
+++ CS4125Project/src/cs4125project/CS4125Project.java
@@ -7,13 +7,6 @@ package cs4125project;
 
 import Database.DBControler;
 import MenuUI.MainMenuUI;
-import Services.*;
-import Stock.StockItem;
-import MenuUI.PaymentSetupUI;
-import ThirdParty.Payment.AIBPaymentSystem;
-import ThirdParty.Payment.BOIPaymentSystem;
-import ThirdParty.Payment.CardPayment;
-import ThirdParty.Payment.Payment;
 import java.io.IOException;
 
 /**
@@ -26,6 +19,7 @@ public class CS4125Project {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException, InterruptedException {
+
         DBControler.getInstance().load(DBControler.ALL_DB);
         // TODO code application logic here
         MainMenuUI menu = new MainMenuUI();
