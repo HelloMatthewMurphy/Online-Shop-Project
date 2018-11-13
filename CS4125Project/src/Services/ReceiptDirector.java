6f31dbca1f8ca4d271d05e7536e243b3f41cdbb1
JackBarrett <jackbhd96@gmail.com>
2018-11-13 18:19:40 +0000

    Populate Receipt director.

---
+++ CS4125Project/src/Services/ReceiptDirector.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Services;
+
+/**
+ *
+ * @author Jack
+ */
+public class ReceiptDirector {
+ 
+    private static ReceiptDirector instance;
+    
+    public static ReceiptDirector GetInstance(){
+    if(instance == null)
+        instance = new ReceiptDirector();
+    return instance;
+    }
+    
+    public void makeReceipt(ReceiptBuilder receiptBuilder){
+    receiptBuilder.buildusername();
+    receiptBuilder.buildEmail();
+    receiptBuilder.buildPaymentDetails();
+    receiptBuilder.bulidDelivery();
+    receiptBuilder.buildPurchases();
+    }
+        
+}
