18d6026393270a4c1294867e1da06f53e699e5b7
JackBarrett <jackbhd96@gmail.com>
2018-11-13 18:01:25 +0000

    Create most of the required feilds for DetailedReceiptBuilder & PrivateReceiptBuilder.

--- CS4125Project/src/Services/DetailedReceiptBuilder.java
+++ CS4125Project/src/Services/DetailedReceiptBuilder.java
@@ -5,10 +5,50 @@
  */
 package Services;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Jack
  */
 public class DetailedReceiptBuilder {
+    private Receipt receipt;
     
+    public DetailedReceiptBuilder(){
+        this.receipt = new Receipt();
+    }
+    
+    public void buildusername(){
+        receipt.Username(Shop.getInstance().getAccount().getUsername());
+    }
+    
+    public void buildEmail(){
+        receipt.Email(Shop.getInstance().getAccount().getEmail());
+    }
+    
+    //Need brians payment method to add.
+    public void buildPurchaseDetails(){
+        //receipt.PaymentDetails(Shop.getInstance().getAccount());
+    }
+    
+    //Delivery needs to be implemented
+    public void bulidDelivery(){
+        
+    } 
+  
+    public void buildPurchases(){
+        ArrayList<Purchase> basket = new ArrayList<Purchase>();
+        basket = ShoppingBasket.GetInstance().GetBasketContents();
+        String purchase = "You have " + basket.size() + " items.\n";
+        
+        for(int i = 0; i < basket.size(); i++){
+            purchase += (basket.get(i).getItem().getName()) + "\n";
+        }
+        
+        receipt.Purchases(purchase);
+    } 
+  
+    public Receipt getReceipt(){
+        return this.receipt;
+    } 
 }
