6fb93078926f745345e0125b181f7ad4f7d504a1
JackBarrett <jackbhd96@gmail.com>
2018-11-14 14:51:07 +0000

    Set payment info when a payment option is selected

--- CS4125Project/src/ThirdParty/Payment/AIBPaymentSystem.java
+++ CS4125Project/src/ThirdParty/Payment/AIBPaymentSystem.java
@@ -1,10 +1,12 @@
 package ThirdParty.Payment;
 
+import Services.Shop;
 import javax.swing.JOptionPane;
 
 public class AIBPaymentSystem implements IPaymentSystem {
     
     public void processPayment(String paymentSystem) {
+        Shop.getInstance().getAccount().getPaymentType().paymentInfo = "Using Allied Irish Bank gateway for " + paymentSystem;
         JOptionPane.showMessageDialog(null,"Using Allied Irish Bank gateway for " + paymentSystem);
     }
 }
--- CS4125Project/src/ThirdParty/Payment/BOIPaymentSystem.java
+++ CS4125Project/src/ThirdParty/Payment/BOIPaymentSystem.java
@@ -1,10 +1,12 @@
 package ThirdParty.Payment;
 
+import Services.Shop;
 import javax.swing.JOptionPane;
 
 public class BOIPaymentSystem implements IPaymentSystem {
     
     public void processPayment(String paymentSystem){
+        Shop.getInstance().getAccount().getPaymentType().paymentInfo = "Using Bank Of Ireland gateway for " +  paymentSystem;
         JOptionPane.showMessageDialog(null ,"Using Bank Of Ireland gateway for " +  paymentSystem);
     }
 }
